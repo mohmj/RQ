4b423193cdadee4e6a276b35cc6fbdd72d4b7272
mohmj <mohizuu.99@gmail.com>
2024-01-06 16:12:46 +0300

    update

--- RQ/RQ/urls.py
+++ RQ/RQ/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("CustomerApp.urls")),
-    path('orders/',include("OrdersApp.urls"))
+    path('orders',include("OrdersApp.urls"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
